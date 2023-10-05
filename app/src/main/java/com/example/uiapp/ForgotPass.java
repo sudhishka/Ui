7d72615fb1ac2ea750204e6c9064488a2b271562
sudhishka <126661091+sudhishka@users.noreply.github.com>
2023-10-05 23:40:58 +0530

    initial_setup

---
+++ app/src/main/java/com/example/uiapp/ForgotPass.java
@@ -0,0 +1,13 @@
+package com.example.uiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgotPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+    }
+}
